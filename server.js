276d58606c04784edf65fea16de200fe976de1e4
root <root@ip-172-31-26-167.us-west-2.compute.internal>
2016-10-22 13:42:27 +0000

    Fix port

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-var http = require("http");
+// Create object Express
+var express = require('express');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("=)");
-  response.end();
-}).listen(8888);
+// Create to  Express- application
+var app = express();
+
+var currentDir='/home/program/task_02/webserver/';
+
+// http://localhost:80/
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+app.get('/:id', function(req, res){
+    //res.send(req.params.id);
+    res.sendfile(req.params.id);
+});
+
+app.get('/js/:id', function(req, res){
+    res.sendfile(currentDir+'js/'+req.params.id);
+});
+
+app.get('/js/vendor/:id', function(req, res){
+    res.sendfile(currentDir+'js/vendor/'+req.params.id);
+});
+
+app.get('/css/:id', function(req, res){
+    res.sendfile(currentDir+'css/'+req.params.id);
+});
+
+
+app.listen(80);
+
+console.log('Start WEB server!');
